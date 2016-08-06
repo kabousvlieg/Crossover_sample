7227cd552efac80e49d1ddd6ec67d88a9b7966a3
kabousvlieg <kobus@embeddedfool.net>
2016-08-07 01:00:05 +0200

    Project created

--- client/main.cpp
+++ client/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <boost/log/trivial.hpp>
+#include <unistd.h>
+#include <synchapi.h>
+#include "monitor.h"
+
+const int monitorInterval = 1000*1000*60*5; //5 minutes in milliseconds
 
 int main() {
     BOOST_LOG_TRIVIAL(info) << "Client app startup";
-    std::cout << "Hello, World!" << std::endl;
+    while (1)
+    {
+        auto mem = monitor::memory();
+        std::cout << "Memory available = " << mem << std::endl;
+        Sleep(monitorInterval);
+    }
     return 0;
 }
