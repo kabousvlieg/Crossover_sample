6bc3caeafcd3375a484297dee098d5521b113eca
kabousvlieg <kobus@embeddedfool.net>
2016-08-07 18:04:08 +0200

    Added basic tcp ip client server functionality

--- client/main.cpp
+++ client/main.cpp
@@ -3,16 +3,26 @@
 #include <unistd.h>
 #include <synchapi.h>
 #include "monitor.h"
+#include "tcpIpClient.h"
 
-const int monitorInterval = 1000*1000*60*5; //5 minutes in milliseconds
+//const int monitorInterval = 1000*60*5; //5 minutes in milliseconds
+const int monitorInterval = 1000*15; //15 seconds in milliseconds
 
 int main() {
     BOOST_LOG_TRIVIAL(info) << "Client app startup";
-    while (1)
-    {
-        auto mem = monitor::memory();
-        std::cout << "Memory available = " << mem << std::endl;
-        Sleep(monitorInterval);
+    try {
+        while (1)
+        {
+            auto mem = monitor::memory();
+            std::cout << "Memory available = " << mem << std::endl;
+            auto client = new tcpIpClient();
+            client->update();
+            Sleep(monitorInterval);
+        }
+    }
+    catch (std::exception& e) {
+        BOOST_LOG_TRIVIAL(fatal) << "Caught exception:";
+        BOOST_LOG_TRIVIAL(fatal) << e.what() << std::endl;
     }
     return 0;
 }
--- server/main.cpp
+++ server/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <boost/log/trivial.hpp>
+#include "tcpIpServer.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    BOOST_LOG_TRIVIAL(info) << "Server app startup";
+    try {
+        auto server = new tcpIpServer();
+        server->Listen();
+        while (1)
+        {
+        }
+    }
+    catch (std::exception& e) {
+        BOOST_LOG_TRIVIAL(fatal) << "Caught exception:";
+        BOOST_LOG_TRIVIAL(fatal) << e.what() << std::endl;
+    }
     return 0;
 }
